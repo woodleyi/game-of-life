3a5e1c38a95b71761183f3b5e75b709739e3b463
Ian Woodley <ianstonewoodley@gmail.com>
2020-05-29 22:01:27 -0400

    grid rendering

--- ts/game-of-life.ts
+++ ts/game-of-life.ts
@@ -1,5 +1,5 @@
 
-class Renderer {
+class GameRenderer {
     private canvas: HTMLCanvasElement
     private context: CanvasRenderingContext2D
 
@@ -9,6 +9,67 @@ class Renderer {
         this.canvas.height = window.innerHeight
         this.context = this.canvas.getContext('2d') as CanvasRenderingContext2D
     }
+
+    public draw(game: Game, cellSize: number, cellColor: Color) {
+        this.context.fillStyle = cellColor
+        
+        let numHorizontalCells = Math.floor(this.canvas.width / cellSize)
+        let numVerticalCells = Math.floor(this.canvas.height / cellSize)
+
+        for (var row=0; row <= numVerticalCells; row++) {
+            let y = row * cellSize
+            
+            for (var column=0; column <= numHorizontalCells; column++) {
+                let x = column * cellSize
+
+                if (game.cells[row]?.[column]?.isAlive()) {
+                    this.context.fillRect(x, y, cellSize, cellSize)
+                }
+                this.context.strokeRect(x, y, cellSize, cellSize)
+            }
+        }
+    }
 }
 
-new Renderer()
+
+
+enum CellState {
+    ALIVE, DEAD
+}
+
+enum Color {
+    BLACK = "#000000",
+    RED = "#FF0000",
+    GREEN = "#00FF00",
+    BLUE = "#0000FF"
+}
+
+
+class Cell {
+    private state: CellState
+
+    constructor(state: CellState) {
+        this.state = state
+    }
+
+    public isAlive(): boolean {
+        return this.state == CellState.ALIVE
+    }
+}
+
+
+class Game {
+    private _cells: Cell[][] = []
+
+    get cells(): Cell[][] {
+        return this._cells
+    }
+}
+
+
+
+// Start the game
+let game = new Game()
+let renderer = new GameRenderer()
+
+renderer.draw(game, 32, Color.BLACK)
